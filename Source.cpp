e565249767eab4ef4f090fe9ead0334966ce446c
Jaime Barnhill <barnhijl@gmail.com>
2021-03-24 11:06:18 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+// Jaime Dano (Barnhill)
+// CIS 1202 101
+// 3/24/2021
+// Description: A program that says 'Hello World'.
+
+#include <iostream>  //console
+
+using namespace std;
+
+int main() {
+
+	//Print Hello World
+	cout << "Hello World." << endl;
+
+	// END Program
+	system("pause");
+	return 0;
+}
